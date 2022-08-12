340f9be390647ec87ef678a35b6ccf4b206c4f21
Bernardo Gonçalves <bernardomfg@gmail.com>
2022-08-12 01:21:50 +0100

    finished first version of ar-scraping

--- model/File.py
+++ model/File.py
@@ -2,8 +2,10 @@ from mongoengine import *
 
 class File(Document):
     file_url = URLField(required=True)
+    filename = StringField()
     isParsed = BooleanField()
     localFilePath = StringField()
+    created = DateTimeField()
 
 
     
