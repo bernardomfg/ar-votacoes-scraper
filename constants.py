080ba2d658d5d9fd457d040676817ff1c147cd44
Bernardo Gonçalves <bernardomfg@gmail.com>
2024-03-28 00:42:14 +0000

    added range of years to webscrape

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 # Web scraping constants
 AR_ARCHIVE_BASE_URL = "https://www.parlamento.pt/ArquivoDocumentacao/Paginas/Arquivodevotacoes.aspx"
+AR_ARCHIVE_BASE_URL_YEAR_SUFFIX = "?ano="
+AR_ARCHIVE_BASE_URL_YEAR_LOWER_BOUND = 2012
 
 # Regex pattern for filename date extraction
 FILENAME_DATE_PATTERN = r'\d{4}-\d{2}-\d{2}'
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import datetime
 import os
 import dotenv
 import logging
@@ -7,7 +8,7 @@ import mongoengine as mongo
 from modules.database import connect_to_database
 from modules.web_scraping import scrape_webpage, extract_filename_from_url
 from modules.file_processing import process_file
-from constants import AR_ARCHIVE_BASE_URL
+from constants import AR_ARCHIVE_BASE_URL, AR_ARCHIVE_BASE_URL_YEAR_LOWER_BOUND, AR_ARCHIVE_BASE_URL_YEAR_SUFFIX
 
 def main():
 
@@ -24,16 +25,30 @@ def main():
     # Connect to the database
     connect_to_database(dbConnectionString)
 
-    # Scraping the webpage
-    votings = scrape_webpage(AR_ARCHIVE_BASE_URL)
+    # get the years to be scraped
+    currentYear = datetime.now().year
+    lowerBoundYear = AR_ARCHIVE_BASE_URL_YEAR_LOWER_BOUND
+    years = range(lowerBoundYear, currentYear + 1)
 
-    # Process files
-    for voting in votings:
-        url = voting['href']
-        logging.info(f"Fetching {url}")
-        filename = extract_filename_from_url(url)
-        file_path = os.path.join(filesystem_root_path, filename)
-        process_file(url, filename, file_path)
+    # get the url and suffix
+    baseUrl = AR_ARCHIVE_BASE_URL
+    urlSuffix = AR_ARCHIVE_BASE_URL_YEAR_SUFFIX
+
+
+    #iterate over the years to get the votings
+    for year in years:
+         # Scraping the webpage
+        scrapeUrl = baseUrl + urlSuffix + str(year)
+        votings = scrape_webpage(scrapeUrl)
+
+        # Process files
+        for voting in votings:
+            fileUrl = voting['href']
+            logging.info(f"Fetching {fileUrl}")
+            filename = extract_filename_from_url(fileUrl)
+            file_path = os.path.join(filesystem_root_path, filename)
+            process_file(fileUrl, filename, file_path)
+   
 
 if __name__ == "__main__":
     main()
